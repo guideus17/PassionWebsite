98904da3eb85e9eead8f58285d85063298fb019d
Guilherme Deus <guil0294@stud.kea.dk>
2021-09-08 13:46:00 +0200

    responsive song page done

--- js/index.js
+++ js/index.js
@@ -1,63 +1,62 @@
-
-const url= "https://songs-9c7e.restdb.io/rest/music?";
+const url = "https://songs-9c7e.restdb.io/rest/music?";
 
 const options = {
     headers: {
-        "x-apikey":"61376b4343cedb6d1f97edd7",
+        "x-apikey": "61376b4343cedb6d1f97edd7",
     },
 };
 
 fetch(url, options)
-.then((response) => {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response.json();
-})
+    .then((response) => {
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    })
 
-.then((data)=> {
-    handData(data);
-    console.log(data);
-})
-.catch((e)=> {
-    console.error("An error occured:", e.message);
-});
+    .then((data) => {
+        handData(data);
+        console.log(data);
+    })
+    .catch((e) => {
+        console.error("An error occured:", e.message);
+    });
 
 
 
 
 function handData(data) {
-    
-    
-        data.forEach(showsongs);
-    }
 
-    function showsongs(song){
-        console.log(song);
-        }
+
+    data.forEach(showsongs);
+}
+
+function showsongs(song) {
+    console.log(song);
+}
 // -------------------------------------Clone template and put it in place---------------------------------
 
 function showsongs(song) {
     // grab template and cloone it
-const templateSelection = document.querySelector(".allsongs").content;
-const cloneOfTemplate = templateSelection.cloneNode(true);
+    const templateSelection = document.querySelector(".allsongs").content;
+    const cloneOfTemplate = templateSelection.cloneNode(true);
 
 
-// change content
+    // change content
 
-cloneOfTemplate.querySelector("h1").textContent = `${song.song}`;
-cloneOfTemplate.querySelector(".songcoverimg").src = `${song.img}`;
-cloneOfTemplate.querySelector("h2").textContent = `${song.artist}`;
+    cloneOfTemplate.querySelector("h1").textContent = `${song.song}`;
+    cloneOfTemplate.querySelector(".songcoverimg").src = `${song.img}`;
+    cloneOfTemplate.querySelector("h2").textContent = `${song.artist}`;
 
-cloneOfTemplate.querySelector("a").href = `songpage.html${song.artist}`;
+    cloneOfTemplate.querySelector("a").href = `songpage.html${song.artist}`;
 
-cloneOfTemplate.querySelector("a").setAttribute("href","songpage.html?song=" + song._id);
+    cloneOfTemplate.querySelector("a").setAttribute("href", "songpage.html?song=" + song._id);
 
 
 
-// put clone in the place
-const GridWithsongs = document.querySelector("main");
-GridWithsongs.appendChild(cloneOfTemplate);
+    // put clone in the place
+    const GridWithsongs = document.querySelector("main");
+    GridWithsongs.appendChild(cloneOfTemplate);
 
 
 };
@@ -68,9 +67,9 @@ document.querySelector("#hamb").addEventListener("click", hambani)
 
 
 
-    
-  function hambani() {
-   
+
+function hambani() {
+
     console.log("open");
     document.querySelector("nav").classList.toggle("showup");
 
@@ -78,8 +77,3 @@ document.querySelector("#hamb").addEventListener("click", hambani)
     document.querySelector("#two").classList.toggle("twoactive");
     document.querySelector("#three").classList.toggle("threeactive");
 }
-
-
-
-
-
